f2c4c4cd6076f67e0f0ec641d1832a3b1a0739e2
Foo Bar <foo@bar.baz>
2023-02-18 14:47:58 -0700

    [wip] parasite library

--- libs/parasite/autogen/mpox.erl
+++ libs/parasite/autogen/mpox.erl
@@ -26,4 +26,49 @@ fmk() ->
     maps:keys(file_map()).
 
 
-%parse(#{<<
+%% -record(ep,
+%%         {url       :: binary(),
+%%          method    :: binary(),
+%%          params    :: binary(),
+%%          responses :: map()}).
+
+-spec test_path() -> map().
+
+test_path() ->
+    M = file_map(),
+    P = maps:get(<<"paths">>, M),
+    X = maps:filter(
+            fun
+                (<<"/accounts/{pubkey}/transactions/pending">>, _) ->
+                    true;
+                (_, _) ->
+                    false
+            end,
+            P),
+    X.
+
+parse_paths(Map) ->
+    lists:map(fun parse/1, maps:to_list(Map)).
+
+parse({URL, Map}) ->
+    Map2
+        = #{method     := _,
+            parameters := _,
+            responses  := _}
+        = parse2(Map),
+    Map3 = maps:put(url, URL, Map2),
+    Map3.
+
+
+parse2(#{<<"get">> := Map}) ->
+    Map2
+        = #{parameters := _,
+            responses  := _}
+        = parse3(Map),
+    Map3 = maps:put(method, get, Map2),
+    Map3.
+
+parse3(#{<<"parameters">> := P,
+         <<"responses">>  := R}) ->
+    #{parameters => P,
+      responses  => R}.
