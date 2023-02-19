c3e617bf65630d3d579e22b0db37e408c76e586b
Foo Bar <foo@bar.baz>
2023-02-19 13:48:27 -0700

    [wip] progress on parasite library

--- libs/parasite/autogen/mpox.erl
+++ libs/parasite/autogen/mpox.erl
@@ -49,18 +49,47 @@ test_paths() ->
 
 
 
--record(ep,
+-record(ep_get,
         {function_name  :: binary(),
          description    :: binary(),
          parameters     :: [ParamTypeName :: binary()],
          response_types :: [{HttpCode         :: integer(),
-                             ResponseTypeName :: binary(),
+                             ResponseType     :: {ref, TypeName :: binary()}
+                                               | {inline, Type :: map()},
                              ResponseDesc     :: binary()}],
-         url            :: binary(),
-         method         :: get | post | delete}).
+         url            :: binary()}).
 
 
--type endpoint() :: #ep{}.
+-record(ep_post,
+        {function_name     :: binary(),
+         description       :: binary(),
+         parameters        :: [ParamTypeName :: binary()],
+         request_body_type :: {ref, TypeName :: binary()},
+         response_types    :: [{HttpCode         :: integer(),
+                                ResponseType     :: {ref, TypeName :: binary()}
+                                                  | {inline, Type :: map()}
+                                                  | none,
+                                ResponseDesc     :: binary()}],
+         url               :: binary()}).
+
+
+-record(ep_delete,
+        {function_name     :: binary(),
+         description       :: binary(),
+         parameters        :: [ParamTypeName :: binary()],
+%         request_body_type :: {ref, TypeName :: binary()},
+         response_types    :: [{HttpCode         :: integer(),
+                                ResponseType     :: {ref, TypeName :: binary()}
+                                                  | {inline, Type :: map()}
+                                                  | none,
+                                ResponseDesc     :: binary()}],
+         url               :: binary()}).
+
+
+-type endpoint() :: #ep_get{}
+                  | #ep_post{}
+                  | #ep_delete{}.
+
 
 
 -spec parse_paths(Paths) -> Result
@@ -78,49 +107,108 @@ parse_paths(Map) ->
          Endpoint     :: endpoint().
 
 
-parse({URL, Map}) ->
-    #{function_name  := FunName,
+parse({URL, #{<<"get">> := Map}}) ->
+    #{function_name   := FunName,
        description    := Desc,
        parameters     := Params,
-       response_types := RTs,
-       % url is from input
-       method         := Method} = parse2(Map),
-    #ep{function_name  = FunName,
-        description    = Desc,
-        parameters     = Params,
-        response_types = RTs,
-        url            = URL,
-        method         = Method}.
+       response_types := RTs} = parse2_get(Map),
+    #ep_get{function_name  = FunName,
+            description    = Desc,
+            parameters     = Params,
+            response_types = RTs,
+            url            = URL};
+parse({URL, #{<<"post">> := Map}}) ->
+    #{function_name      := FunName,
+       description       := Desc,
+       parameters        := Params,
+       request_body_type := ReqBodyType,
+       response_types    := RTs} = parse2_post(Map),
+    #ep_post{function_name     = FunName,
+             description       = Desc,
+             parameters        = Params,
+             request_body_type = ReqBodyType,
+             response_types    = RTs,
+             url               = URL}.
 
+%% AHh.... hmm need to think about the keys... this case-splitting thing isn't 
+%parse({URL, #{<<"delete">> := Map}}) ->
+%    #{function_name      := FunName,
+%       description       := Desc,
+%       parameters        := Params,
+%       response_types    := RTs} = parse2_delete(Map),
+%    #ep_delete{function_name     = FunName,
+%               description       = Desc,
+%               parameters        = Params,
+%               request_body_type = ReqBodyType,
+%               response_types    = RTs,
+%               url               = URL}.
+%
 
-parse2(#{<<"get">> := Map}) ->
-    Map2
-        = #{function_name  := _,
-            description    := _,
-            parameters     := _,
-            response_types := _}
-        = parse3(Map),
-    Map3 = maps:put(method, get, Map2),
-    Map3.
-
-
-
-parse3(#{<<"operationId">> := FunctionName,
-         <<"description">> := Desc,
-         <<"parameters">>  := Params,
-         <<"responses">>   := Resps}) ->
+parse2_get(#{<<"operationId">> := FunctionName,
+             <<"description">> := Desc,
+             <<"parameters">>  := Params,
+             <<"responses">>   := Resps}) ->
     #{function_name   => FunctionName,
       description     => Desc,
+      parameters      => parse_parameters(Params),
+      response_types  => parse_response_types(Resps)};
+% no parameters
+parse2_get(#{<<"operationId">> := FunctionName,
+             <<"description">> := Desc,
+             <<"responses">>   := Resps}) ->
+    #{function_name   => FunctionName,
+      description     => Desc,
+      parameters      => [],
+      response_types  => parse_response_types(Resps)};
+% no description
+parse2_get(#{<<"operationId">> := FunctionName,
+             <<"parameters">>  := Params,
+             <<"responses">>   := Resps}) ->
+    #{function_name   => FunctionName,
+      description     => <<"(no description given)">>,
       parameters      => parse_parameters(Params),
       response_types  => parse_response_types(Resps)}.
 
 
+parse2_post(#{<<"operationId">> := FunctionName,
+              <<"description">> := Desc,
+              <<"parameters">>  := Params,
+              <<"requestBody">> := RB,
+              <<"responses">>   := Resps}) ->
+    #{function_name     => FunctionName,
+      description       => Desc,
+      parameters        => parse_parameters(Params),
+      request_body_type => parse_request_body_type(RB),
+      response_types    => parse_response_types(Resps)};
+%% no parameters field
+parse2_post(#{<<"operationId">> := FunctionName,
+              <<"description">> := Desc,
+              <<"requestBody">> := RB,
+              <<"responses">>   := Resps}) ->
+    #{function_name     => FunctionName,
+      description       => Desc,
+      parameters        => [],
+      request_body_type => parse_request_body_type(RB),
+      response_types    => parse_response_types(Resps)}.
+
+
+parse_request_body_type(#{<<"content">> := #{<<"application/json">> := #{<<"schema">> := #{<<"$ref">> := <<"#/components/schemas/", Name/binary>>}}}}) ->
+    {ref, Name}.
 
 parse_parameters(Params) ->
     lists:map(fun parse_parameter/1, Params).
 
+
+
+-spec parse_parameter(ParamMap) -> Param
+    when ParamMap :: map(),
+         Param    :: {ref, Name :: binary()}
+                   | {inline, Type :: map()}.
+
 parse_parameter(#{<<"$ref">> := <<"#/components/parameters/", ParamTypeName/binary>>}) ->
-    ParamTypeName.
+    {ref, ParamTypeName};
+parse_parameter(Map) when is_map(Map) ->
+    {inline, Map}.
 
 
 
@@ -130,8 +218,11 @@ parse_response_types(Types) ->
 parse_response_type({HttpCode_bin, #{<<"content">>    := #{<<"application/json">> := #{<<"schema">> := #{<<"$ref">> := <<"#/components/schemas/", ResponseTypeName/binary>>}}},
                                      <<"description">> := ResponseDesc}}) ->
     {erlang:binary_to_integer(HttpCode_bin),
-     ResponseTypeName,
-     ResponseDesc}.
+     {ref, ResponseTypeName},
+     ResponseDesc};
+% no response success type
+parse_response_type({<<"200">>, #{<<"description">> := ResponseDesc}}) ->
+    {200, none, ResponseDesc}.
 
 
 %% current status:
