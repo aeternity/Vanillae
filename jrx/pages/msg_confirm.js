0f1477230296337585bd14ef8e80eae4306e68d4
Peter <peter.harpending@gmail.com>
2023-09-06 17:20:35 -0600

    "confirm message signature" popup not seeing bg message for some reason
    further evidence for my belief that this "ask the user about signing things"
    feature is unnecessary

--- jrx/pages/msg_confirm.js
+++ jrx/pages/msg_confirm.js
@@ -3,23 +3,12 @@ var result = null;
 document.getElementById('good').onclick = () => { console.log('click good'); result = 'good' }
 document.getElementById('bad').onclick = () => { console.log('click bad'); result = 'bad' }
 
-browser.runtime.onMessage.addListener((msg) => {
-    console.error("ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRROR");
-    console.log('msg', msg);
-    //document.getElementById("message").innerHTML = msg_str;
+// for some reason the listener here never gets triggered
+async function listener(msg, _sender, _sendResponse) {
+    console.log('listener triggered');
+    console.log(msg);
+}
 
-    //const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-
-    //while (true) {
-    //    if (result === 'good') {
-    //        console.log('good');
-    //        return true;
-    //    }
-    //    else if (result === 'bad') {
-    //        console.log('bad');
-    //        return false;
-    //    }
-    //    else
-    //        await sleep(100);
-    //}
-});
+console.log('adding listener');
+browser.runtime.onMessage.addListener(listener);
+console.log('added listener');
